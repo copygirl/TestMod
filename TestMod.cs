6753e440f2f1fcf7c8d8d4b68ddde1eba820917d
copygirl <halfnerd.copygirl@gmail.com>
2017-11-30 16:17:48 +0100

    Initial commit

---
+++ TestMod.cs
@@ -0,0 +1,20 @@
+using System;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace TestMod
+{
+	public class TestMod : ModBase
+	{
+		public override void StartServerSide(ICoreServerAPI api)
+		{
+			
+		}
+		
+		public override void StartClientSide(ICoreClientAPI api)
+		{
+			
+		}
+	}
+}
